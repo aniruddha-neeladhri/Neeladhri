0ff46ba2ae0d2a49ecf74c8883d8a3d20055acb5
Srivalli-Yaarlagadda <srivalli@geekonomy.in>
2026-07-28 09:27:08 +0530

    updated font colors in collection n ph number

--- components/collections/livingroom.tsx
+++ components/collections/livingroom.tsx
@@ -155,7 +155,7 @@ export default function LivingRoom() {
     <div className="mt-2 lg:mt-16 select-none">
       <Typography
         variant="display-2xl"
-        className={`text-center ${theme === "luxury" ? "text-[#D3B898] font-cormorant-garamond font-normal" : "text-[#555555] font-cormorant-garamond font-medium uppercase"}`}
+        className={`text-center ${theme === "luxury" ? "text-[#FFFFFF] font-cormorant-garamond font-normal" : "text-[#555555] font-cormorant-garamond font-medium uppercase"}`}
       >
         Living
       </Typography>
